317975ebaf34a9d542608557e6bfffc13e459ca3
muratcpt <muratcpt332@gmail.com>
2023-12-14 21:22:08 +0300

    Project Files

---
+++ Assets/Scripts/PLayerLife.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PLayerLife : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody2D rb2d;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            Die();
+        }
+    }
+
+
+    private void Die()
+    {
+        rb2d.bodyType = RigidbodyType2D.Static;
+        anim.SetTrigger("death");
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
